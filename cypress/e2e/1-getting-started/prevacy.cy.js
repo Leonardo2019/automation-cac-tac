4f539610a7b177bd553732fcc926ef30aa8a7999
Leonardo Fagner Pires Soares <leonardofps@gmail.com>
2022-10-20 19:47:41 -0300

    Cenário de repetição

--- cypress/e2e/1-getting-started/prevacy.cy.js
+++ cypress/e2e/1-getting-started/prevacy.cy.js
@@ -1,9 +1,12 @@
 
 
-it('Testar a página da politica de privacidade', () => {
+Cypress._.times(5, () => {
+    
+it('Testar a página da politica de privacidade e repetir 5 vezes', () => {
            
     cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/privacy.html')
  
     cy.contains('Talking About Testing').should('be.visible')
          
  })
+})
